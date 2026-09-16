7ab9eb1f88dfc708ac2765d84d07645a8fb85726
Ujjwal Singh <157193430+aakansh-kun@users.noreply.github.com>
2026-09-16 22:15:34 +0530

    Checking and setting kth bit

--- Intermediate/Bit_Manipulation/bits.java
+++ Intermediate/Bit_Manipulation/bits.java
@@ -1,15 +1,28 @@
 package Intermediate.Bit_Manipulation;
 
-//get 3rd bit(position = 2) of a number n
+//get 3rd bit(position = 2) of a number n (perform and)
 public class bits {
     public static void main(String[] args) {
         int n = 5;
         int pos = 2;
-        int bitMast = 1 << pos;
-        if ((n & bitMast) == 0) {
+        int bitMask = 1 << pos;
+        if ((n & bitMask) == 0) {
             System.out.println("Bit is 0");
         } else {
             System.out.println("Bit is 1");
         }
+
+        // set 2nd bit (position = 1) of number n (perform or)
+        int pos1 = 1;
+        int bitMask1 = 1 << pos1;
+        n = n | bitMask1;
+        System.out.println(n);
+
+        // clear 3rd bit (position = 2) of number n (perform and not)
+        int n2 = 5;
+        int pos2 = 2;
+        int bitMask2 = 1 << pos2;
+        n2 = n2 & ~bitMask2;
+        System.out.println(n2);
     }
 }
