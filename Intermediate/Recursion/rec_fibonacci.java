42cd5411d41e59b25a1466b781d6d61a5f6073c7
Ujjwal Singh <157193430+aakansh-kun@users.noreply.github.com>
2026-09-22 23:48:55 +0530

    Fibonacci of Nth number By recusion

---
+++ Intermediate/Recursion/rec_fibonacci.java
@@ -0,0 +1,16 @@
+package Intermediate.Recursion;
+
+public class rec_fibonacci {
+
+    public static int printFibonacci(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        return printFibonacci(n - 1) + printFibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.print(printFibonacci(7));
+    }
+
+}
